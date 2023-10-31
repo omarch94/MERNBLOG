9064d44ba28ce03ce94ac35586db61114c83b21c
omarch94 <omarcherti@gmail.com>
2023-10-31 14:32:26 +0100

    Secure our app

--- server/app.js
+++ server/app.js
@@ -3,12 +3,32 @@ const express=require('express');
 const connectDb=require('./config/connectDb');
 const {errorHandler,notFound} = require('./middlewares/error');
 const cors=require("cors");
+// xss 
+const xss=require("xss-clean")
+//rate limiting
+const rateLimiting=require("express-rate-limit")
+const helmet=require('helmet')
+const hpp=require("hpp")
 // PORT
 const port=process.env.PORT||8002;
 require('dotenv').config();
 const app=express();
 //MIDLLEWARES
 app.use(express.json())
+// prevent xss atack
+app.use(xss())
+// rate limiting
+app.use(rateLimiting({
+
+    windowMs:10*60*1000, // 10 minutes
+    max:2,
+}
+    ))
+
+    // security headers (helmet)
+app.use(helmet())
+//prevent http param pollutioon
+app.use(hpp())
 //CORS Policy
 app.use(cors({
     origin:"http://localhost:3000"
